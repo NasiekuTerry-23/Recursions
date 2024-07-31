4e6848393405dcd425a9ab27b487ac4c16f88e09
Graduate School Lab <gradlab@ku.ac.ke>
2024-07-31 13:06:53 +0300

    UserInputs in Kotlin

--- Expressions/src/Main.kt
+++
@@ -1,8 +0,0 @@
-fun main(){
-
-    val total : Int = (1..500).random() + 202
-    val numberCheck = if(total > 500) "More than 500" else "Less than 500"
-
-    println("total = $total")
-    println("total = $numberCheck")
-}
---
+++ UserInput/src/Main.kt
@@ -0,0 +1,6 @@
+fun main(){
+    print("Enter some text: ")
+    val stringInput = readLine()!!
+    println("You wrote: $stringInput")
+
+}
