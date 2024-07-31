343f8253e156b591a4e12d4d2435a1ca4de94bf8
Graduate School Lab <gradlab@ku.ac.ke>
2024-07-31 12:46:44 +0300

    Expressions

--- Conversion/src/Main.kt
+++
@@ -1,8 +0,0 @@
-fun main(){
-    print("Enter a Number: ")
-    val number = readLine()!!.toInt()
-    print("Enter another Number: ")
-    val number2 = readLine()!!.toInt()
-
-    println("$number + $number2 = ${number +number2}")
-}
